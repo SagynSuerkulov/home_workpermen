fe8c21ff18fdaba047f5cc05923a97c17021f75e
SagynSuerkulov <132682444+SagynSuerkulov@users.noreply.github.com>
2023-06-07 21:04:18 +0600

    Add files via upload

---
+++ lesson_16.dart
@@ -0,0 +1,18 @@
+// task 1 
+// void main() {
+//   double b = 4.20;
+//   int a = 3;
+//   print(a + b);
+// }
+// task 2
+// void main() {
+//   String text = 'Name';
+//   String texT = 'flutter';
+//   print('i want to learn flutter');
+// }
+// task 3
+// void main() {
+//   double myNumber = 2.225;
+//   String myText = '';
+//   print('$myNumber  $myText');
+// }
